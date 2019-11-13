ec6ee25e6d609d376f973daf9c66a3c702e0d33b
Mariana Chahrouri <chahm222@newschool.edu>
2019-11-13 18:37:01 -0500

    commit

--- Wk11/gulpfile.js
+++ Wk11/gulpfile.js
@@ -14,6 +14,12 @@ const autoprefixer = require('gulp-autoprefixer');
 //step2
 const browserSync = require('browser-sync').create();
 
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+
+
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
@@ -23,12 +29,15 @@ gulp.task('browserSync', function() {
     })
 })
 
-
-
 gulp.task('sassworkflow', function () {
     gulp.src('./src/sass/**/*.scss')
     // tasks go here
     .pipe(sourcemaps.init())
+    .pipe(babel({
+        // transpile ES6 to ES5 and minify
+        presets: ['@babel/preset-env'] // bug with
+        minify - doesnt reload
+        }))
     .pipe(sass().on('error', sass.logError))
     .pipe(cssnano())
     .pipe(autoprefixer({
@@ -42,7 +51,28 @@ gulp.task('sassworkflow', function () {
     }));
 });
 
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
-gulp.task('default',['browserSync','sassworkflow'], function() {
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src([
+        './src/js/*.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(sourcemaps.write())
+        .pipe(rename('scripts.min.js'))
+        .pipe(browserSync.reload({
+             stream: true
+         }))
+});
+
+gulp.task('default',['browserSync', 'sassworkflow', 'lint', 'scripts'], function() {
     gulp.watch('./src/sass/**/*.scss', ['sassworkflow']);
+    gulp.watch('./src/js/*.js', ['lint', 'scripts']);
 })
